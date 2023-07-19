eb6db9a063e2bf2d6bd9e3fe1433d4eb6a949671
Salil Upadhyay <upsalil.01@gmail.com>
2023-07-19 22:53:29 +0530

    added routing

--- client/src/app/routing/app-routing.module.ts
+++ client/src/app/routing/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from '../pages/homepage/homepage.component';
+import { SigninComponent } from '../pages/signin/signin.component';
+import { SignupComponent } from '../pages/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomepageComponent,
+  },
+  {
+    path: 'signin',
+    component: SigninComponent,
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
