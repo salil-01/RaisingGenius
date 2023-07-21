a5ed80d1a318e4608192bab6d44183237f9c7358
Salil Upadhyay <upsalil.01@gmail.com>
2023-07-21 19:15:54 +0530

    ai model working fine

--- server/api/routes/chat_route.py
+++ server/api/routes/chat_route.py
@@ -1,58 +1,52 @@
 from flask import Blueprint, request, jsonify, current_app
 from auth.auth_decorator import authenticate_and_authorize
 chat_bp = Blueprint('/chat', __name__)
+import json
+import openai
+import difflib
 
+with open('data.json', 'r') as json_file:
+    data = json.load(json_file)
 
-def get_parental_advice(chat_history, user_query):
+def get_response(user_query):
     try:
         apiKey = current_app.config['OPENAI_API_KEY']
+        
         # Create a list of messages with user queries and model responses for warm-up
-        warmup_prompt = [
-            {'role': 'system', 'content': 'Welcome to the Parental Advisor! How can I assist you today?'}
-        ]
+        warmup_prompt = {'role': 'system', 'content': 'You are a parental advisor assisting parents with their queries and concerns. Your goal is to provide personalized, heart-to-heart responses based on their questions and experiences. Your responses should be in the same tone and language as the examples provided earlier, creating a sense of connection with the parents seeking advice.Please keep the responses concise and focused on addressing the specific query and should be under 50-60, do not go more than that. Avoid using lengthy explanations or bullet points to ensure the responses feel more natural and human-like. You can use the stored data as inspiration, but make sure to generate unique and original content to provide the best guidance to the parents.Remember to be empathetic, understanding, and supportive in your replies, acknowledging the challenges parents face and offering practical solutions with a touch of warmth and care.Whenever a user sends a query, you can start your response by empathizing with their situation and then proceed to provide guidance and suggestions based on their specific concern. Be sure to tailor your response to the users query, making it relevant and meaningful to their situation.Let your responses reflect the heart-to-heart connection that parents seek in their quest for advice, and always prioritize the well-being of the child and fostering a positive and nurturing environment.'}
+        
+        response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[warmup_prompt,
+            {"role": "user", "content": user_query}
+        ],
+        api_key=apiKey
+       )
+        intial_res = response.choices[0].message.content
+        return intial_res
 
-        # Create a list of messages with user queries and model responses from the chat history
-        messages = warmup_prompt + [{'role': 'user', 'content': user_query}]
-        for i in range(0, len(chat_history), 2):
-            # User queries
-            messages.append({'role': 'user', 'content': chat_history[i]})
-            # Model responses
-            messages.append({'role': 'system', 'content': chat_history[i+1]})
 
-        # Make API call to GPT-3
-        response = openai.Completion.create(
-            engine="text-davinci-002",  # GPT-3 engine name
-            messages=messages,
-            # Adjust this value as per your token limit (free tier has 4096 tokens)
-            max_tokens=100,
-            # Specify stopping criteria for the response
-            stop=["\n", "example"],
-        )
+        # most_similar_query = difflib.get_close_matches(user_query, [entry['query'] for entry in data])[0]
+        # response = [entry['answer'] for entry in data if entry['query'] == most_similar_query][0]
+        # ai_response = ai_model.generate_response(response)
 
-        return response.choices[0].text.strip()  # Extract the response text
+        # return response.choices[0].text.strip()  # Extract the response text
     except Exception as e:
         return str(e)
 
 
 @chat_bp.route('/chat', methods=['POST'])
 @authenticate_and_authorize()
-def get_advice():
-    return jsonify({"msg":"Chat Route"})
-    # try:
-    #     data = request.json
-    #     user_query = data['query']
+def chat():
+    try:
+        data = request.json
+        user_query = data['query']
 
-    #     # Load or retrieve the chat history for the user (you can use a database for this)
-    #     # For simplicity, I'm using a list in memory here
-    #     chat_history = data.get('chat_history', [])
+        # Load or retrieve the chat history for the user (you can use a database for this)
+        # For simplicity, I'm using a list in memory here
 
-    #     # Get the model's response
-    #     response = get_parental_advice(chat_history, user_query)
-
-    #     # Store the current user query and model response in the chat history
-    #     chat_history.append(user_query)
-    #     chat_history.append(response)
-
-    #     return jsonify({'response': response, 'chat_history': chat_history})
-    # except Exception as e:
-    #     return jsonify({'error': str(e)})
+        # Get the model's response
+        response = get_response(user_query)
+        return jsonify({'response': response})
+    except Exception as e:
+        return jsonify({'error': str(e)})
